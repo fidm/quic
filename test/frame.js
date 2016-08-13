ac80eec9e6911a4a177022012615d48edbfd2d59
zensh <admin@zensh.com>
2016-08-13 23:37:48 +0800

    add BlockedFrame, StopWaitingFrame, PingFrame

--- test/frame.js
+++ test/frame.js
@@ -130,4 +130,66 @@ tman.suite('QuicFrame', function () {
         WindowUpdateFrame.fromBuffer(windowUpdateFrame.toBuffer()))
     })
   })
+
+  tman.suite('BlockedFrame', function () {
+    const BlockedFrame = QuicFrame.BlockedFrame
+
+    tman.it('new BlockedFrame with StreamId(0)', function () {
+      let streamId = QuicID.StreamId.fromId(0)
+      let blockedFrame = new BlockedFrame(streamId)
+
+      assert.strictEqual(blockedFrame.type, 5)
+      assert.ok(blockedFrame.toBuffer().equals(bufferFromBytes([
+        0x05,
+        0x00, 0x00, 0x00, 0x00
+      ])))
+      assert.deepEqual(blockedFrame,
+        BlockedFrame.fromBuffer(blockedFrame.toBuffer()))
+    })
+  })
+
+  tman.suite('StopWaitingFrame', function () {
+    const StopWaitingFrame = QuicFrame.StopWaitingFrame
+
+    tman.it('new StopWaitingFrame', function () {
+      let packetNumber = new QuicID.PacketNumber(bufferFromBytes([0xff, 0x1f]))
+      let stopWaitingFrame = new StopWaitingFrame(packetNumber)
+
+      assert.strictEqual(stopWaitingFrame.type, 6)
+      assert.ok(stopWaitingFrame.toBuffer().equals(bufferFromBytes([
+        0x06,
+        0xff, 0x1f
+      ])))
+      assert.deepEqual(stopWaitingFrame,
+        StopWaitingFrame.fromBuffer(stopWaitingFrame.toBuffer(), packetNumber.byteLen))
+    })
+  })
+
+  tman.suite('PingFrame', function () {
+    const PingFrame = QuicFrame.PingFrame
+
+    tman.it('new PingFrame', function () {
+      let pingFrame = new PingFrame()
+
+      assert.strictEqual(pingFrame.type, 7)
+      assert.ok(pingFrame.toBuffer().equals(bufferFromBytes([0x07])))
+      assert.deepEqual(pingFrame, PingFrame.fromBuffer(pingFrame.toBuffer()))
+    })
+  })
+
+  tman.suite('CongestionFrame', function () {
+    const CongestionFrame = QuicFrame.CongestionFrame
+
+    tman.it('new CongestionFrame', function () {
+      let congestionFrame = new CongestionFrame(0b00100000)
+
+      assert.strictEqual(congestionFrame.type, 32)
+      assert.ok(congestionFrame.toBuffer().equals(bufferFromBytes([0b00100000])))
+      assert.deepEqual(congestionFrame, CongestionFrame.fromBuffer(congestionFrame.toBuffer()))
+    })
+
+    tman.it('when invalid CongestionFrame type', function () {
+      assert.throws(() => new CongestionFrame(0b01100000), /INVALID_FRAME_DATA/)
+    })
+  })
 })
