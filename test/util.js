42208c2a63b0ed0a3aef2bd066978bb8867b5f5e
zensh <admin@zensh.com>
2016-08-14 18:44:28 +0800

    remove Node.js v4 support

--- test/util.js
+++ test/util.js
@@ -3,17 +3,14 @@
 //
 // **License:** MIT
 
-const util = require('../lib/util')
-
-exports.allocBuffer = util.allocBuffer
 exports.bufferFromBytes = function (array) {
   let bytes = []
   if (!Array.isArray(array)) array = [array]
   for (let val of array) {
     if (typeof val !== 'string') bytes.push(val)
     else {
-      for (let byte of new Buffer(val, 'utf8').values()) bytes.push(byte)
+      for (let byte of Buffer.from(val, 'utf8').values()) bytes.push(byte)
     }
   }
-  return new Buffer(bytes)
+  return Buffer.from(bytes)
 }
